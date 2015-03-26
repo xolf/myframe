fc10652a2272b6da1926b97df1659442f85c7a6a
Florian <paypal.xolf@yahoo.com>
2015-03-26 16:07:53 +0100

    0.4.1
    + Function(view404)
    + Global(boolean[VIEW404])

--- index.php
+++ index.php
@@ -8,11 +8,20 @@ define('ROUTE',$route);
 
 $varRoute = explode('/', ROUTE);
 
-function getView($view){
+function getView($view, $record = TRUE){
 
 	$view = str_replace('.','/',$view);
 
 	return 'views/'.$view.'.php';
+
+	if($record) define('VIEW404',FALSE);
+
+}
+
+function view404($view)
+{
+
+	if(VIEW404) return getView($view);
 
 }
 
